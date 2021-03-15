033f04a828b36a464ef3bbc7333d73d4b7a5798a
bill <billyray@stratiumsoftware.com>
2021-03-15 17:47:02 +0800

    prod

---
+++ build/precache-manifest.4799b31fa9e445531d6152b1a0c47718.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbfd924a172c61f7977a482fd318925d",
+    "url": "/eversure4/index.html"
+  },
+  {
+    "revision": "49254c90184c9d98a37e",
+    "url": "/eversure4/static/css/3.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "272806e0ef426cb9bcb7",
+    "url": "/eversure4/static/css/main.556094b7.chunk.css"
+  },
+  {
+    "revision": "49254c90184c9d98a37e",
+    "url": "/eversure4/static/js/3.4b07b6ae.chunk.js"
+  },
+  {
+    "revision": "834c18ea823c536e6c919a7759b4dd86",
+    "url": "/eversure4/static/js/3.4b07b6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c332d8e7c57d2cacc5b8",
+    "url": "/eversure4/static/js/4.4d7658fa.chunk.js"
+  },
+  {
+    "revision": "7eaf54ee93d6800bb190",
+    "url": "/eversure4/static/js/5.6d3e4d14.chunk.js"
+  },
+  {
+    "revision": "5827cae8dfe02009a3cd",
+    "url": "/eversure4/static/js/6.8a6fc50c.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/eversure4/static/js/6.8a6fc50c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272806e0ef426cb9bcb7",
+    "url": "/eversure4/static/js/main.73d7c2bd.chunk.js"
+  },
+  {
+    "revision": "8b52252acdb3babe2814",
+    "url": "/eversure4/static/js/runtime-main.a37c7e27.js"
+  },
+  {
+    "revision": "4fc533242df56e0a46fa",
+    "url": "/eversure4/static/js/xlsx.6bae1302.chunk.js"
+  },
+  {
+    "revision": "98f0cacc6bb63b64b98aac7cac082d27",
+    "url": "/eversure4/static/media/NotoSans-Bold.98f0cacc.ttf"
+  },
+  {
+    "revision": "a1375023bb3f9d55dfc6d58685cd6e69",
+    "url": "/eversure4/static/media/NotoSans-BoldItalic.a1375023.ttf"
+  },
+  {
+    "revision": "afe1714f485b424f221147e1be6cf8ad",
+    "url": "/eversure4/static/media/NotoSans-Italic.afe1714f.ttf"
+  },
+  {
+    "revision": "5a87cce84010f7cea085ae218d44a64b",
+    "url": "/eversure4/static/media/NotoSans-Regular.5a87cce8.ttf"
+  },
+  {
+    "revision": "3ada7a9482cb9a123ad501e45053adb3",
+    "url": "/eversure4/static/media/Oxygen-Bold.3ada7a94.ttf"
+  },
+  {
+    "revision": "dfefd7db6a479b1161e9f0f9cb0abb8d",
+    "url": "/eversure4/static/media/Oxygen-Light.dfefd7db.ttf"
+  },
+  {
+    "revision": "61d9daf063ba38f2d05f8adb7267e6fd",
+    "url": "/eversure4/static/media/Oxygen-Regular.61d9daf0.ttf"
+  },
+  {
+    "revision": "e99cef05e658d250c27513f3cf33ec04",
+    "url": "/eversure4/static/media/eversure_logo.e99cef05.png"
+  }
+]);
